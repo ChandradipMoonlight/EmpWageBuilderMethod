c11794357b58b7645e7fd34d00de091b9402c23d
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-16 08:35:08 +0530

    Computing daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,9 @@ package empWageBuilder;
 public class EmpWage {
 	public static void main(String[] args) {
 		int IS_FULL_TIME=1;
+		int WAGE_PER_HR=20;
+		int emphr=0;
+		int dailyWage=0;
 		
 
 		System.out.println("Wellcome to the Employee Wage computation program.");
@@ -11,9 +14,13 @@ public class EmpWage {
 		
 		if (checkEmp==IS_FULL_TIME) {
 			System.out.println("Employee is Present");
+			emphr=8;
 		} else {
 			System.out.println("Employee is Absent");
+			emphr=0;
 		}
+		dailyWage=emphr*WAGE_PER_HR;
+		System.out.println("Daily wage = "+dailyWage);
 	
 		
 	}
