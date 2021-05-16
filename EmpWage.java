b69172f9468a8cb75733b88d1b13b2f559dc298f
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-16 09:45:28 +0530

    computing wage for month

--- EmpWage.java
+++ EmpWage.java
@@ -5,17 +5,22 @@ public class EmpWage {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int WAGE_PER_HR=20;
+	public static final int MAX_WORKING_DAYS=20;
 	
 	public static void main(String[] args) {
 		
 		int empHr=0;
 		int dailyWage=0;
+		int totalWage=0;
 		
 
 		System.out.println("Wellcome to the Employee Wage computation program.");
 		
-		int checkEmp = (int) (Math.random()*3);
 		
+		for (int i =1; i<=MAX_WORKING_DAYS; i++) {
+			
+			int checkEmp = (int) (Math.random()*3);
+			
 		switch (checkEmp) {
 		case IS_FULL_TIME :
 			empHr=8;
@@ -28,8 +33,10 @@ public class EmpWage {
 		}
 		
 		dailyWage=empHr*WAGE_PER_HR;
-		System.out.println("Daily wage = "+dailyWage);
-	
+		totalWage += dailyWage;
 		
+		}
+		System.out.println("Total wage for month = "+totalWage);
 	}
+	
 }
