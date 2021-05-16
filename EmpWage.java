24522fefb712aaf4dfd30fb55d4f08d5e9338500
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-16 09:23:16 +0530

    solve using switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,10 +1,13 @@
 package empWageBuilder;
 
 public class EmpWage {
+	
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int WAGE_PER_HR=20;
+	
 	public static void main(String[] args) {
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
-		int WAGE_PER_HR=20;
+		
 		int empHr=0;
 		int dailyWage=0;
 		
@@ -13,16 +16,17 @@ public class EmpWage {
 		
 		int checkEmp = (int) (Math.random()*3);
 		
-		if (checkEmp==IS_FULL_TIME) {
-			
+		switch (checkEmp) {
+		case IS_FULL_TIME :
 			empHr=8;
-		} else if (checkEmp==IS_PART_TIME) {
+			break;
+		case IS_PART_TIME :
 			empHr=4;
-		}
-		else {
-			
+			break;
+		default :
 			empHr=0;
 		}
+		
 		dailyWage=empHr*WAGE_PER_HR;
 		System.out.println("Daily wage = "+dailyWage);
 	
