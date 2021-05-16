1b17f78bc8bb6c887ece43d788b98b4c525afc37
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-16 08:20:23 +0530

    Checkin Attendace of Emp

--- EmpWage.java
+++ EmpWage.java
@@ -2,8 +2,19 @@ package empWageBuilder;
 
 public class EmpWage {
 	public static void main(String[] args) {
+		int IS_FULL_TIME=1;
+		
 
 		System.out.println("Wellcome to the Employee Wage computation program.");
+		
+		int checkEmp = (int) (Math.random()*2);
+		
+		if (checkEmp==IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
+	
+		
 	}
-
 }
