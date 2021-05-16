98733ab7ef79ea6f69998b8f30c30f096f7947ac
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-16 09:15:31 +0530

    part time employee added

--- EmpWage.java
+++ EmpWage.java
@@ -3,23 +3,27 @@ package empWageBuilder;
 public class EmpWage {
 	public static void main(String[] args) {
 		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 		int WAGE_PER_HR=20;
-		int emphr=0;
+		int empHr=0;
 		int dailyWage=0;
 		
 
 		System.out.println("Wellcome to the Employee Wage computation program.");
 		
-		int checkEmp = (int) (Math.random()*2);
+		int checkEmp = (int) (Math.random()*3);
 		
 		if (checkEmp==IS_FULL_TIME) {
-			System.out.println("Employee is Present");
-			emphr=8;
-		} else {
-			System.out.println("Employee is Absent");
-			emphr=0;
+			
+			empHr=8;
+		} else if (checkEmp==IS_PART_TIME) {
+			empHr=4;
 		}
-		dailyWage=emphr*WAGE_PER_HR;
+		else {
+			
+			empHr=0;
+		}
+		dailyWage=empHr*WAGE_PER_HR;
 		System.out.println("Daily wage = "+dailyWage);
 	
 		
