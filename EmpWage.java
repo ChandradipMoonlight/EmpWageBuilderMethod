14b25b11e50c873105014ea27881afa51655cabb
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-18 17:15:59 +0530

    calculating wages for multiple companies

--- EmpWage.java
+++ EmpWage.java
@@ -4,7 +4,6 @@ public class EmpWage {
 	
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-
 	
 	public static int calculateEmpWageForCompany(String company, int wagePerHr, int maxWorkingHr, int maxWorkingDays) {
 		
@@ -12,10 +11,7 @@ public class EmpWage {
 		int totalWorkingDays=0;
 		int totalHr=0;
 		
-
 		System.out.println("Wellcome to the Employee Wage computation program.");
-		
-		
 		
 		while (totalWorkingDays<maxWorkingDays && totalHr<maxWorkingHr) {
 			int empHr=0;
@@ -42,18 +38,10 @@ public class EmpWage {
 		totalWage=totalHr*wagePerHr;
 		System.out.println("Total Employee Wage for company:"+company+" is "+totalWage);
 		return totalWage;
-		
-		
 	}
-	
-	
 	public static void main(String[] args) {
 		calculateEmpWageForCompany("Demart", 100, 208, 26);
 		calculateEmpWageForCompany("Reliance", 120, 208, 26);
-		calculateEmpWageForCompany("zfTech", 150, 200, 25);
-		
-		
-		
+		calculateEmpWageForCompany("zfTech", 150, 200, 25);	
 	}
-	
 }
