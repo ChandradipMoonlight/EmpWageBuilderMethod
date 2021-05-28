22264e565d904dc2d6c51441d6d2402895badb3d
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-28 17:17:14 +0530

    wage for multiple company using array

--- EmpWage.java
+++ EmpWage.java
@@ -1,53 +1,30 @@
 package empWageBuilder;
 
 public class EmpWage {
+	public final  String company;
+	public int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
+	public int totalEmpWage;
 	
-	public static final int IS_FULL_TIME=1;
-	public static final int IS_PART_TIME=2;
-	
-	private final  String company;
-	private final int empRatePerHr;
-	private final int maxWorkingDays;
-	private final int maxWorkingHrPerMonth;
-	private int totalEmpWage;
-	
-	public EmpWage(String company, int empRatePerHr, int maxWorkingDays, int maxWorkingHrPerMonth) {
+	public EmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		this.company = company;
-		this.empRatePerHr = empRatePerHr;
-		this.maxWorkingDays = maxWorkingDays;
-		this.maxWorkingHrPerMonth = maxWorkingHrPerMonth;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
 	}
-	public void calculateEmpWageForCompany() {
-		int totalWorkingDays=0;
-		int totalHr=0;
-		int empHr;
-		while (totalWorkingDays<maxWorkingDays && totalHr<maxWorkingHrPerMonth) {
-			empHr=0;
-			totalWorkingDays++;
-			int checkEmp = (int) (Math.random()*3);
-		switch (checkEmp) {
-		case IS_FULL_TIME :
-			empHr=8;
-			break;
-		case IS_PART_TIME :
-			empHr=4;
-			break;
-		default :
-			empHr=0;
-		}
-		totalHr += empHr;
 		
-		int dailyWage=empHr*empRatePerHr;
-		System.out.println("Daily Wage ="+dailyWage);
-		
+		public void setTotalEmpWage(int totalEmpWage) {
+			this.totalEmpWage = totalEmpWage;
 		}
-		totalEmpWage=totalHr*empRatePerHr;
-		System.out.println("Total Employee Wage for company:"+company+" is "+totalEmpWage);
-	}
-	public static void main(String[] args) {
-		EmpWage dMart = new EmpWage("DMart", 50, 5, 26);
-		EmpWage reliance = new EmpWage("reliance", 20, 5,25);
-		dMart.calculateEmpWageForCompany();
-		reliance.calculateEmpWageForCompany();
-	}
+		@Override
+		public String toString() {
+			return "Total Emp Wage for company: "+company+" is: "+totalEmpWage;
+		}
+		public static void main(String[] args) {
+			EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+			empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+			empWageBuilder.addCompanyEmpWage("Reliance", 10, 5, 20);
+			empWageBuilder.computeEmpWage();
+		}
 }
