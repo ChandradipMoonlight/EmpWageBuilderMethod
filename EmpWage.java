8a650f3da4bc1cca0374fe16e173bd938f5dc4c5
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-03 22:22:12 +0530

    get total emp wage when queried by company

--- EmpWage.java
+++ EmpWage.java
@@ -31,5 +31,6 @@ public class EmpWage {
 			empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
 			empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
 			empWageBuilder.computeEmpWage();
+			System.out.println("Total wage for Dmart company: "+empWageBuilder.getTotalWage("Dmart"));
 		}
 }
